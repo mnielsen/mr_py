78d6b3987d9edf7d69bb56bda702bdd511c66d17
Michael Nielsen <mn@michaelnielsen.org>
2009-03-06 23:29:42 +0000

    Adding test directory

---
+++ mr/test/test.py
@@ -0,0 +1,6 @@
+# test.py
+#
+# Runs the suite of test programs for the mr.py library
+
+import test_map
+import test_mapreduce
---
+++ mr/test/test_map_fn.py
@@ -0,0 +1,6 @@
+# test_map_fn.py
+#
+# Defines the mapper function used by test_map.py
+# See that file for more information.
+
+def mapper(key,value,params): return key**2
